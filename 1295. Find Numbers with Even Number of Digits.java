08b055fb67bce9c9ef1e33b5e169a5ad08645823
Dhinesh S <dhinesh22012007@gmail.com>
2026-06-27 20:53:38 +0530

    Add explanation comment for even digit count function
    Added a comment explaining the function's input and output.

---
+++ 1295. Find Numbers with Even Number of Digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int digit=0;
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            int temp=nums[i];
+            while(temp>0){
+                count++;
+                temp/=10;
+            }
+            if(count%2==0){
+                digit++;
+            }
+        }
+        return digit;
+    }
+}
+/*Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation: 
+12 contains 2 digits (even number of digits). 
+345 contains 3 digits (odd number of digits). 
+2 contains 1 digit (odd number of digits). 
+6 contains 1 digit (odd number of digits). 
+7896 contains 4 digits (even number of digits). 
+Therefore only 12 and 7896 contain an even number of digits.*/
