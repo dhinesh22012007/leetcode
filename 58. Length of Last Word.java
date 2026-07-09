2abfbde71d40969199daa7ff6f3c62fe73e63967
Dhinesh S <dhinesh22012007@gmail.com>
2026-07-09 20:28:38 +0530

    Add problem statement and examples as comments
    Added a comment block explaining the problem statement and examples for the length of the last word in a string.

---
+++ 58. Length of Last Word.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int i=s.length()-1;
+        int n=0;
+        while(i>=0)
+        {
+            if(s.charAt(i)==' '){
+                  break;
+            }
+             n++;
+            i--;
+           
+        }
+        return n;
+    }
+}
+
+/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.*/
