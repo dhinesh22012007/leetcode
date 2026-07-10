f4bd22ec0fbbb7f6189a57ed232fa8b85dcf1898
Dhinesh S <dhinesh22012007@gmail.com>
2026-07-10 20:47:55 +0530

    Add comments for clarity in toLowerCase method
    Added comments to explain the function's purpose and examples.

---
+++ 709. To Lower Case.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String toLowerCase(String s) {
+        s=s.toLowerCase();
+        return s;
+    }
+}
+
+/*Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"*/
